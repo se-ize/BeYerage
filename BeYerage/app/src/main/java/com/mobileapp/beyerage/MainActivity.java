2044667b931706f80083532b1e3fe92d91c0d3a9
leyujin1007 <59291066+lmyourGenie@users.noreply.github.com>
2021-09-01 22:41:59 +0900

    Revert App

--- BeYerage/app/src/main/java/com/mobileapp/beyerage/MainActivity.java
+++ BeYerage/app/src/main/java/com/mobileapp/beyerage/MainActivity.java
@@ -1,18 +1,8 @@
 package com.mobileapp.beyerage;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -25,28 +15,25 @@ import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.mobileapp.beyerage.dto.Beverage;
 import com.mobileapp.beyerage.network.BeverageAPI;
 import com.mobileapp.beyerage.network.BeverageAPIController;
 import com.mobileapp.beyerage.shop.ShopService;
-import retrofit2.Call;
 
-import java.io.IOException;
-import java.lang.reflect.Method;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Locale;
-import java.util.Set;
-import java.util.UUID;
+
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity{
     //컨트롤러
@@ -65,26 +52,6 @@ public class MainActivity extends AppCompatActivity{
     private String userVoice = "";
     //로그 확인용
     private String tag = "MainActivity";
-
-    /**
-     * 블루투스용
-     */
-
-    String TAG = "MainActivity";
-    UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
-
-    TextView textStatus;
-    Button btnParied, btnSearch, btnSend;
-    ListView listView;
-
-    BluetoothAdapter btAdapter;
-    Set<BluetoothDevice> pairedDevices;
-    ArrayAdapter<String> btArrayAdapter;
-    ArrayList<String> deviceAddressArray;
-
-    private final static int REQUEST_ENABLE_BT = 1;
-    BluetoothSocket btSocket = null;
-    ConnectedThread connectedThread;
 
     //해시키 발급
     public static String getKeyHash(final Context context) {
@@ -117,39 +84,12 @@ public class MainActivity extends AppCompatActivity{
         //해시키 로그에 출력
         Log.e("GR_KeyHash",getKeyHash(MainActivity.this));
 
-        /**
-         * 블루투스용  https://popcorn16.tistory.com/192
-         */
-
-        // variables
-        textStatus = (TextView) findViewById(R.id.text_status);
-        btnParied = (Button) findViewById(R.id.btn_paired);
-        btnSearch = (Button) findViewById(R.id.btn_search);
-        btnSend = (Button) findViewById(R.id.btn_send);
-        listView = (ListView) findViewById(R.id.listview);
-        listView.setOnItemClickListener(new myOnItemClickListener());
-
-        listView.setOnItemClickListener(new myOnItemClickListener());
-
         // Get permission
         String[] permission_list = {
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         };
         ActivityCompat.requestPermissions(MainActivity.this, permission_list, 1);
-
-        // enable bluetooth
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (!btAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-        // Show paired devices
-        btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
-        deviceAddressArray = new ArrayList<>();
-        listView.setAdapter(btArrayAdapter);
-
-        listView.setOnItemClickListener(new myOnItemClickListener());
 
         /* TTS, STT */
 
@@ -407,108 +347,6 @@ public class MainActivity extends AppCompatActivity{
                 shopService.defaultGuidance(tts);
             }
         });
-    }
-
-    /**
-     * 블루투스 연결용
-     * */
-
-    public void onClickButtonPaired(View view){
-        //btArrayAdapter.clear();
-        if(deviceAddressArray!=null && !deviceAddressArray.isEmpty()){ deviceAddressArray.clear(); }
-        pairedDevices = btAdapter.getBondedDevices();
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                btArrayAdapter.add(deviceName); // deviceName은 btArrayAdapter에, devicesHardwareAddress는 deviceAddressArray에 저장
-                deviceAddressArray.add(deviceHardwareAddress);
-            }
-        }
-    }
-
-    public void onClickButtonSearch(View view){
-        // Check if the device is already discovering
-        if(btAdapter.isDiscovering()){
-            btAdapter.cancelDiscovery();
-        } else {
-            if (btAdapter.isEnabled()) {
-                btAdapter.startDiscovery();
-                btArrayAdapter.clear();
-                if (deviceAddressArray != null && !deviceAddressArray.isEmpty()) {
-                    deviceAddressArray.clear();
-                }
-                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-                registerReceiver(receiver, filter);
-            } else {
-                Toast.makeText(getApplicationContext(), "bluetooth not on", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    // Send string "a"
-    public void onClickButtonSend(View view){
-        if(connectedThread!=null){ connectedThread.write("a"); }
-    }
-
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                btArrayAdapter.add(deviceName);
-                deviceAddressArray.add(deviceHardwareAddress);
-                btArrayAdapter.notifyDataSetChanged();
-            }
-        }
-    };
-    public class myOnItemClickListener implements AdapterView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
-
-            textStatus.setText("try...");
-
-            final String name = btArrayAdapter.getItem(position); // get name
-            final String address = deviceAddressArray.get(position); // get address
-            boolean flag = true;
-
-            BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-            // create & connect socket
-            try {
-                btSocket = createBluetoothSocket(device);
-                btSocket.connect();
-            } catch (IOException e) {
-                flag = false;
-                textStatus.setText("connection failed!");
-                e.printStackTrace();
-            }
-
-            // start bluetooth communication
-            if(flag){
-                textStatus.setText("connected to "+name);
-                connectedThread = new ConnectedThread(btSocket);
-                connectedThread.start();
-            }
-        }
-    }
-
-    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
-        try {
-            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
-            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
-        } catch (Exception e) {
-            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
-        }
-        return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
     }
 
     @Override
