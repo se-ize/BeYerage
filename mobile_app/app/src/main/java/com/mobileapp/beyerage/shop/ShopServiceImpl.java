1d6bddeccf389f7f8df0dd779142c43beb9dda3f
leyujin1007 <59291066+lmyourGenie@users.noreply.github.com>
2021-10-09 14:32:58 +0900

    tts set

--- mobile_app/app/src/main/java/com/mobileapp/beyerage/shop/ShopServiceImpl.java
+++ mobile_app/app/src/main/java/com/mobileapp/beyerage/shop/ShopServiceImpl.java
@@ -52,10 +52,12 @@ public class ShopServiceImpl extends MainActivity implements ShopService {
         Log.d("가까운 편의점 안내 access: ", msg);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             //QUEUE_FLUSH: Queue 값을 초기화한 후 값을 넣는다.
-            tts.speak("현재 위치기반 가장 가까운 편의점은," + msg + "  해당 편의점 내의 음료 위치 안내는 하단의 좌측 버튼," +
+            tts.speak("현재 위치기반 가장 가까운 편의점은," + msg + " 편의점 문을 열고 앞으로 직진하시면 정면에 음료수 냉장고가 있습니다." +
+                    " 해당 편의점 내의 음료 위치 안내는 하단의 좌측 버튼," +
                     "가장 인기 있는 음료 안내는 하단의 우측 버튼을 터치해주세요.", TextToSpeech.QUEUE_FLUSH, null, null);
         } else {
-            tts.speak("현재 위치기반 가장 가까운 편의점은," + msg + "  해당 편의점 내의 음료 위치 안내는 하단의 좌측 버튼," +
+            tts.speak("현재 위치기반 가장 가까운 편의점은," + msg + " 편의점 문을 열고 앞으로 직진하시면 정면에 음료수 냉장고가 있습니다." +
+                    " 해당 편의점 내의 음료 위치 안내는 하단의 좌측 버튼," +
                     "가장 인기 있는 음료 안내는 하단의 우측 버튼을 터치해주세요.", TextToSpeech.QUEUE_FLUSH, null);
         }
     }
