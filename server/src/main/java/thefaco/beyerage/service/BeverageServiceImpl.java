f16811d79b70b2a494864c9faa7e0fe5d7c3a301
CokeLee777 <akak640@kyonggi.ac.kr>
2021-10-22 12:51:05 +0900

    서버 소스코드 리팩터링
    소스코드의 가독성과 성능 향상을 위해 소스코드의 서비스, 컨트롤러 를 스프링 데이터 JPA 기반의 소스코드로 변경하였습니다.
    
    #5

--- server/src/main/java/thefaco/beyerage/service/BeverageServiceImpl.java
+++ server/src/main/java/thefaco/beyerage/service/BeverageServiceImpl.java
@@ -8,93 +8,77 @@ import thefaco.beyerage.domain.BottleType;
 import thefaco.beyerage.repository.BeverageRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class BeverageServiceImpl implements BeverageService {
 
-    private final BeverageRepository beverageRepository;
+    private final BeverageRepository beverageJpaRepository;
 
     //음료 추가
     @Override
     @Transactional
     public Long addBeverage(Beverage beverage) {
-        beverageRepository.save(beverage);
+        beverageJpaRepository.save(beverage);
         return beverage.getId();
-    }
-
-    //모든 음료 상세정보 객체 조회
-    @Override
-    public List<Beverage> findBeverages() {
-        return beverageRepository.findAll();
     }
 
     //모든 음료 상세정보와 위치정보 객체 조회
     @Override
     public List<Beverage> findBeveragesWithLoc() {
-        return beverageRepository.findAllWithLoc();
+        return beverageJpaRepository.findAllWithLoc();
     }
 
     //PK값으로 음료 상세정보 조회
     @Override
     public Beverage findOneById(Long id) {
-        return beverageRepository.findById(id);
-    }
-
-    //이름으로 음료 상세정보 조회
-    @Override
-    public List<Beverage> findOneByName(String name) {
-        return beverageRepository.findByName(name);
+        Optional<Beverage> findBeverage = beverageJpaRepository.findById(id);
+        return findBeverage.orElse(null);   //반환할 객체가 없다면 null 반환
     }
 
     //이름으로 음료 상세정보와 위치정보 조회
     @Override
-    public List<Beverage> findOneByNameWithLoc(String name) {
-        return beverageRepository.findByNameWithLoc(name);
+    public Optional<Beverage> findOneByNameWithLoc(String name) {
+        return beverageJpaRepository.findByNameWithLoc(name);
     }
 
     //음료 수정
     @Override
     @Transactional
     public void updateBeverage(Long id, String name, int price, BottleType type, int size, int row, int column) {
-        Beverage findBeverage = beverageRepository.findByIdWithLoc(id).get(0);
-        findBeverage.updateBeverage(
-                findBeverage, name, price, type, size, row, column);
+        beverageJpaRepository.findByIdWithLoc(id).ifPresent(
+                findBeverage -> findBeverage.updateBeverage(findBeverage, name, price, type, size, row, column)
+        );
     }
 
     //Frequency 값 1 증가 메서드
     @Override
     @Transactional
     public void addFrequency(String name) {
-        Beverage findBeverage = beverageRepository.findByName(name).get(0);
-        findBeverage.addFrequency(findBeverage, findBeverage.getFrequency());
+        beverageJpaRepository.findByNameWithLoc(name).ifPresent(
+                findBeverage -> findBeverage.addFrequency(findBeverage, findBeverage.getFrequency())
+        );
     }
 
     //음료 삭제
     @Override
     @Transactional
     public void deleteBeverage(Beverage beverage) {
-        beverageRepository.delete(beverage);
-    }
-
-    //Frequency 값이 가장 높은 음료 하나를 조회하는 메서드
-    @Override
-    public List<Beverage> findMostFreqOne() {
-        return beverageRepository.findMostFreq();
+        beverageJpaRepository.delete(beverage);
     }
 
     //Frequency 값이 가장 높은 음료 상세정보와 위치정보를 조회하는 메서드
     @Override
-    public List<Beverage> findMostFreqOneWithLoc() {
-        return beverageRepository.findMostFreqWithLoc();
+    public Optional<Beverage> findMostFreqOneWithLoc() {
+        return beverageJpaRepository.findMostFreqWithLoc();
     }
 
     //음료 위치가 겹치는지 여부를 확인하는 메서드
-   @Override
+    @Override
     public Beverage checkDuplicateRowColumn(int row, int column) {
-        List<Beverage> beverages = beverageRepository.findByRowAndColumn(row, column);
-        if(!beverages.isEmpty()) return beverages.get(0);
-        else return null;
+        Optional<Beverage> findBeverage = beverageJpaRepository.findByRowAndColumn(row, column);
+        return findBeverage.orElse(null);
     }
 }
