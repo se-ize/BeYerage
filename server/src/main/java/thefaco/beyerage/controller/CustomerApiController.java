e7b4c8926c95919414198d24053316301a743f1a
CokeLee777 <akak640@kyonggi.ac.kr>
2021-10-31 16:38:53 +0900

    서버부분 고객의소리 소스코드 수정
    고객의 소리를 사용자가 추가하였을 때 모바일 앱쪽으로 Response Body에 JSON 데이터를 넘겨주는 형식으로 변경하였습니다.

--- server/src/main/java/thefaco/beyerage/controller/CustomerApiController.java
+++ server/src/main/java/thefaco/beyerage/controller/CustomerApiController.java
@@ -2,10 +2,12 @@ package thefaco.beyerage.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import thefaco.beyerage.domain.Customer;
+import thefaco.beyerage.dto.view.CustomerListDto;
 import thefaco.beyerage.service.CustomerService;
 
 @Slf4j
@@ -15,11 +17,15 @@ public class CustomerApiController {
 
     private final CustomerService customerService;
 
-    @PostMapping("/addCustomerVoice")
-    public void addCustomerVoice(@RequestBody String text){
+    @PostMapping(value = "/addCustomerVoice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerListDto addCustomerVoice(@RequestBody String text){
         Customer newCustomer = Customer.createCustomer(text);
         Long savedId = customerService.addText(newCustomer);
 
+        CustomerListDto customerListDto = new CustomerListDto(savedId, text);
+
         log.info("Add new text savedId={}", savedId);
+
+        return customerListDto;
     }
 }
